6f62106d8c1192b4721a973a8cf392aa0afe8bf8
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-12 14:45:30 -0300

    Configurando o Resend 2

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Settings:
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "")
     RESEND_API_KEY: str = os.getenv("RESEND_API_KEY", "")
     MAIL_FROM: str = os.getenv("MAIL_FROM", "")
     FRONTEND_URL: str = os.getenv("FRONTEND_URL", "http://127.0.0.1:5500")
